63f6dd85656b5c8c97b2b7d69faa00614bc4e109
Song <songhan@stanford.edu>
2016-08-02 16:10:05 -0700

    Update gen_squeezeNet.py

--- gen_squeezeNet.py
+++ gen_squeezeNet.py
@@ -5,7 +5,7 @@ Generate the SqueezeNet network.
 If you find SqueezeNet useful in your research, please consider citing the SqueezeNet paper:
 
 @article{SqueezeNet,
-    Author = {Forrest N. Iandola and Matthew W. Moskewicz and Khalid Ashraf and Song Han and William J. Dally and Kurt Keutzer},
+    Author = {Forrest N. Iandola and Song Han and Matthew W. Moskewicz and Khalid Ashraf and William J. Dally and Kurt Keutzer},
     Title = {SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and <1MB model size},
     Journal = {arXiv:1602.07360},
     Year = {2016}
